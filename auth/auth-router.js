44e729ba4906b47a1917533a0ff6ce13960a9b15
James Wigdor <jpwigdor@gmail.com>
2019-06-11 08:13:26 -0400

    Completed guided demo

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +19,40 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", restricted, (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "There was an error" });
+      }
+
+      res.end();
+    });
+  } else {
+    res.end();
+  }
 });
 
 module.exports = router;
